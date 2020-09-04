5696297091cfc23c7dfccd95d63dc730a58beea0
zbelial <zjytj@qq.com>
2020-09-04 23:09:36 +0800

    The real initial commit - working now.

--- posframe-plus.el
+++ posframe-plus.el
@@ -7,7 +7,7 @@
 ;; Copyright (C) 2020, zbelial, all rights reserved.
 ;; Created: 2020-09-04 14:08:12
 ;; Version: 0.1
-;; URL: https://github.com/zbelial/posframe-plus.el
+;; URL: https://github.com/zbelial/posframe-plus
 ;; Keywords:
 ;; Compatibility: GNU Emacs 27.0.50
 ;;
@@ -35,26 +35,29 @@
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
 
+(require 'cl-lib)
 (require 'posframe)
 
-(defcustom posframe-plus-hide-after-move t
-  "When non-nil, hide posframe when moving cursor."
-  :group 'posframe-plus
-  :type 'boolean)
 
-(defvar posframe-plus-last-point 0
+(defvar posframe-plus-active-map
+  (let ((keymap (make-sparse-keymap)))
+    keymap)
+  "Keymap that is enabled when showing a posframe.")
+
+(defvar posframe-plus--last-point 0
   "Hold last point when show tooltip, use for hide tooltip after move point.")
-(defvar posframe-plus-last-scroll-offset 0
+(defvar posframe-plus--last-scroll-offset 0
   "Hold last scroll offset when show tooltip, use for hide tooltip after window scroll.")
 
+(defvar-local posframe-plus--buffer-or-name nil)
 
-(defvar-local posframe-plus-keymap nil)
+(defvar-local posframe-plus--keymap nil)
 
 (defvar posframe-plus-emulation-alist '((t . nil)))
 
 (defun posframe-plus-enable-overriding-keymap (keymap)
   (posframe-plus-uninstall-map)
-  (setq posframe-plus-keymap keymap))
+  (setq posframe-plus--keymap keymap))
 
 (defun posframe-plus-ensure-emulation-alist ()
   (unless (eq 'posframe-plus-emulation-alist (car emulation-mode-map-alists))
@@ -64,8 +67,8 @@
 
 (defun posframe-plus-install-map ()
   (unless (or (cdar posframe-plus-emulation-alist)
-              (null posframe-plus-keymap))
-    (setf (cdar posframe-plus-emulation-alist) posframe-plus-keymap)))
+              (null posframe-plus--keymap))
+    (setf (cdar posframe-plus-emulation-alist) posframe-plus--keymap)))
 
 (defun posframe-plus-uninstall-map ()
   (setf (cdar posframe-plus-emulation-alist) nil))
@@ -79,5 +82,100 @@
 (defun posframe-plus-deactivate-map()
   (posframe-plus-uninstall-map))
 
+
+(defun posframe-plus-hide-frame ()
+  (interactive)
+  (remove-hook 'post-command-hook 'posframe-plus-hide-after-move)
+  (posframe-plus-deactivate-map)
+  (posframe-hide posframe-plus--buffer-or-name)
+  )
+
+(defun posframe-plus-hide-after-move ()
+  (ignore-errors
+    (when (get-buffer posframe-plus--buffer-or-name)
+      (unless (and
+               (equal (point) posframe-plus--last-point)
+               (equal (window-start) posframe-plus--last-scroll-offset))
+        (posframe-plus-hide-frame)))))
+
+(cl-defun posframe-plus-show (buffer-or-name enable-ctrl-g hide-after-move
+                                             &key
+                                             string
+                                             position
+                                             poshandler
+                                             width
+                                             height
+                                             min-width
+                                             min-height
+                                             x-pixel-offset
+                                             y-pixel-offset
+                                             left-fringe
+                                             right-fringe
+                                             internal-border-width
+                                             internal-border-color
+                                             font
+                                             foreground-color
+                                             background-color
+                                             respect-header-line
+                                             respect-mode-line
+                                             respect-tab-line
+                                             initialize
+                                             no-properties
+                                             keep-ratio
+                                             lines-truncate
+                                             override-parameters
+                                             timeout
+                                             refresh
+                                             accept-focus
+                                             hidehandler
+                                             &allow-other-keys)
+  "Pop up a posframe and show STRING at POSITION.
+If enable-ctrl-g is t, pressing `Ctrl-g' will hide the posframe.
+If hide-after-move is t, after moving point, the posframe will hide.
+"
+  (setq posframe-plus--buffer-or-name buffer-or-name)
+
+  (if enable-ctrl-g
+      (define-key posframe-plus-active-map (kbd "C-g") 'posframe-plus-hide-frame)
+    (define-key posframe-plus-active-map (kbd "C-g") nil))
+  (posframe-plus-activate-map posframe-plus-active-map)
+
+  (if hide-after-move
+      (add-hook 'post-command-hook 'posframe-plus-hide-after-move)
+    (remove-hook 'post-command-hook 'posframe-plus-hide-after-move))
+
+  (setq posframe-plus--last-point (point))
+  (setq posframe-plus--last-scroll-offset (window-start))
+
+  (posframe-show buffer-or-name
+                 :string string
+                 :position position
+                 :poshandler poshandler
+                 :width width
+                 :height height
+                 :min-width min-width
+                 :min-height min-height
+                 :x-pixel-offset x-pixel-offset
+                 :y-pixel-offset y-pixel-offset
+                 :left-fringe left-fringe
+                 :right-fringe right-fringe
+                 :internal-border-width internal-border-width
+                 :internal-border-color internal-border-color
+                 :font font
+                 :foreground-color foreground-color
+                 :background-color background-color
+                 :respect-header-line respect-header-line
+                 :respect-mode-line respect-mode-line
+                 :respect-tab-line respect-tab-line
+                 :initialize initialize
+                 :no-properties no-properties
+                 :keep-ratio keep-ratio
+                 :lines-truncate lines-truncate
+                 :override-parameters override-parameters
+                 :timeout timeout
+                 :refresh refresh
+                 :accept-focus accept-focus
+                 :hidehandler hidehandler
+                 ))
 
 (provide 'posframe-plus)
